e590457cf8a0ed8896e1499c814a0827fd52acdf
Chirag <chiragj2019@gmail.com>
2024-01-01 21:27:50 +0530

    initial commit

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,10 @@
 # Program to display the Fibonacci sequence up to n-th term
 
-nterms = int(input("How many terms? "))
+nterms = int(input("No of terms ? "))
 
 # first two terms
 n1, n2 = 0, 1
 count = 0
-
 # check if the number of terms is valid
 if nterms <= 0:
    print("Please enter a positive integer")
